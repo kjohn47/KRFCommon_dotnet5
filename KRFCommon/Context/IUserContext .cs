fab8b373e629b97a05657eebe79e933962e2be2f
Joao Morgado <joaomorgado0@gmail.com>
2019-12-20 12:25:29 +0000

    Add Token User Context Common Data

---
+++ KRFCommon/Context/IUserContext .cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KRFCommon.Context
+{
+    public interface IUserContext
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Claims Claim { get; set; }       
+    }
+}
