12188582e8503981931d23d5201517db55e4b19c
Joao Morgado <joaomorgado0@gmail.com>
2020-01-07 11:28:02 +0000

    Add common db querycommand result

---
+++ KRFCommon/Database/QueryCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace KRFCommon.Database
+{
+    public class QueryCommand
+    {
+        public QueryResult Result { get; set; }
+        public string ResultDescription { get; set; }
+    }
+
+    public enum QueryResult
+    {
+        Success,
+        Error
+    }
+}
