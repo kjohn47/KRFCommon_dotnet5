2ce405eaeca2d087c5f0d3a1c91b73dff37594f3
Joao Morgado <joaomorgado0@gmail.com>
2021-02-05 18:56:09 +0000

    +Add: added new models for settings and database helper for injection

---
+++ KRFCommon/Api/AppConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace KRFCommon.Api
+{
+    public class AppConfiguration
+    {
+        public string ApiName { get; set; }
+        public string TokenIdentifier { get; set; }
+        public string TokenKey { get; set;  }
+    }
+}
---
+++ KRFCommon/Api/RequestContext.cs
@@ -0,0 +1,9 @@
+﻿namespace KRFCommon.Api
+{
+    public class RequestContext
+    {
+        public bool EnableRead { get; set; }
+        public bool MemBufferOnly  { get; set; }
+        public string BufferSize  { get; set; }
+}
+}
